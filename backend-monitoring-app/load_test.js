860001226e16ca9db15c199ac9573c6ad36422b1
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2024-10-24 23:38:28 +0530

    Improve load cases

--- backend-monitoring-app/load_test.js
+++ backend-monitoring-app/load_test.js
@@ -4,10 +4,11 @@ import { sleep, check } from "k6";
 export let options = {
 	stages: [
 		{ duration: "10s", target: 50 }, // Ramp up to 50 users over 30 seconds
-		{ duration: "10s", target: 2000 }, // Stay at 50 users for 1 minute
-		{ duration: "10s", target: 0 }, // Ramp down to 0 users
+		{ duration: "120s", target: 2000 }, // Stay at 50 users for 1 minute
+		{ duration: "60s", target: 0 }, // Ramp down to 0 users
 		{ duration: "10s", target: 5 }, // Ramp up to 50 users over 30 seconds
-		{ duration: "10s", target: 1000 },
+		{ duration: "120s", target: 1000 },
+		{ duration: "120s", target: 10000 },
 	],
 };
 
