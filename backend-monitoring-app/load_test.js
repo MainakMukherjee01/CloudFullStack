ee1fad4b24bd94ad4777c17181ff46bd7dc12fc5
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2024-10-25 22:09:36 +0530

    Reduce TimeScale

--- backend-monitoring-app/load_test.js
+++ backend-monitoring-app/load_test.js
@@ -4,19 +4,19 @@ import { sleep, check } from "k6";
 export let options = {
 	stages: [
 		{ duration: "10s", target: 50 }, // Ramp up to 50 users over 10 seconds
-		{ duration: "600s", target: 50 }, // Stay at 50 users for 10 minutes
-		{ duration: "60s", target: 2000 }, // Ramp up to 2000 users over 1 minute
-		{ duration: "600s", target: 2000 }, // Stay at 2000 users for 10 minutes
-		{ duration: "60s", target: 0 }, // Ramp down to 0 users over 1 minute
-		{ duration: "600s", target: 0 }, // Stay at 50 users over 10 minutes
-		{ duration: "60s", target: 5000 }, // Ramp up to 5000 users over 1 minute
-		{ duration: "600s", target: 5000 }, // Stay at 5000 users for 10 minutes
-		{ duration: "60s", target: 0 }, // Ramp down to 0 users over 1 minute
-		{ duration: "600s", target: 0 }, // Stay at 0 users for 10 minutes
-		{ duration: "60s", target: 10000 }, // Ramp up to 10000 users over 1 minute
-		{ duration: "600s", target: 10000 }, // Stay at 10000 users for 10 minutes
-		{ duration: "60s", target: 0 }, // Ramp down to 0 users over 1 minute
-		{ duration: "600s", target: 0 }, // Stay at 0 users for 10 minutes
+		{ duration: "120s", target: 50 }, // Stay at 50 users for 10 minutes
+		{ duration: "10s", target: 2000 }, // Ramp up to 2000 users over 1 minute
+		{ duration: "120s", target: 2000 }, // Stay at 2000 users for 10 minutes
+		{ duration: "10s", target: 0 }, // Ramp down to 0 users over 1 minute
+		{ duration: "120s", target: 0 }, // Stay at 50 users over 10 minutes
+		{ duration: "10s", target: 5000 }, // Ramp up to 5000 users over 1 minute
+		{ duration: "120s", target: 5000 }, // Stay at 5000 users for 10 minutes
+		{ duration: "10s", target: 0 }, // Ramp down to 0 users over 1 minute
+		{ duration: "120s", target: 0 }, // Stay at 0 users for 10 minutes
+		{ duration: "10s", target: 10000 }, // Ramp up to 10000 users over 1 minute
+		{ duration: "120s", target: 10000 }, // Stay at 10000 users for 10 minutes
+		{ duration: "10s", target: 0 }, // Ramp down to 0 users over 1 minute
+		{ duration: "120s", target: 0 }, // Stay at 0 users for 10 minutes
 	],
 };
 
